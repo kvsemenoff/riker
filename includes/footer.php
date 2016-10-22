fb6ee8a9438e8b7663c62f976c6d480f173526f0
hatuhov <hatuhov@yandex.ru>
2016-10-22 11:36:20 +0300

    q

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <section class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-7 col-sm-7 col-xs-12 ">
+            <div class="col-md-8 col-sm-8 col-xs-12 ">
 		      <ul class="ah-footer-list">
 		      	<li><a href="#">Каталог товаров</a></li>
 		      	<li><a href="#">Женская обувь</a></li>
@@ -40,19 +40,18 @@
 		      	<li><a href="#">Контакты</a></li>
 		      </ul>
             </div>
-            <div class="col-md-5 col-sm-5 col-xs-12 ">
+            <div class="col-md-4 col-sm-4 col-xs-12 ah-inffot">
 				<div class="ah-logoblock">
 					<div class="ah-logoimg">
-						<img src="img/footer-logo.png" alt="footer-logo">
+						<img src="img/footer-logo.png" class="img-responsive" alt="footer-logo">
 					</div>
 					<span class="ah-logoblock-span"><span>8 (800)</span> 707-01-81</span>
 				</div>
 				<form class="ah-footform">
-					<label>
-						<input type="text" name="poisk">
-						<input type="submit" value="Найти">
-					</label>
+					<input type="text" name="poisk">
+					<input type="submit" value="Найти">
 				</form>
+				<div class="clearfix"></div>
 				<div class="ah-fotbotblock">
 					<div class="ah-fotbotblock1">
 						<span class="ah-fotbotblock1-span">Мы принимаем</span>
@@ -63,12 +62,30 @@
 					<div class="ah-fotbotblock2">
 						<span class="ah-fotbotblock2-span">Мы в</span>
 						<ul class="ah-soc">
-							<li><a href="#" class="ah-vk"></a></li>
-							<li><a href="#" class="ah-in"></a></li>
+							<li>
+								<a href="#"><img src="img/ico-vk.png" alt="ico-vk"></a>
+							</li>
+							<li>
+								<a href="#"><img src="img/ico-insta.png" alt="ico-insta"></a>
+							</li>
 						</ul>
 					</div>
 				</div>
             </div>
         </div>
 	</div>
+</section>
+<section class="footer-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-fotleft">
+					<span class="ah-fotleft-span">2016 © ООО «Рикер Самара»</span>
+				</div>
+				<div class="ah-fotright">
+					<span class="ah-fotright-span">Сделано в WEBBEARS</span>
+				</div>
+			</div>
+        </div>
+    </div>
 </section>
