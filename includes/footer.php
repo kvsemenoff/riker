a6ad87db7a82de31319b125f5e12a65a141c3c95
hatuhov <hatuhov@yandex.ru>
2016-10-22 09:57:09 +0300

    w

---
+++ includes/footer.php
@@ -0,0 +1,74 @@
+<div class="clearfix"></div>
+
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 col-sm-7 col-xs-12 ">
+		      <ul class="ah-footer-list">
+		      	<li><a href="#">Каталог товаров</a></li>
+		      	<li><a href="#">Женская обувь</a></li>
+		      	<li><a href="#">Мужская обувь</a></li>
+		      	<li><a href="#">Сумки</a></li>
+		      	<li><a href="#">Одежда</a></li>
+		      </ul>
+		      <ul class="ah-footer-list">
+		      	<li><a href="#">Сервис</a></li>
+		      	<li><a href="#">Доставка</a></li>
+		      	<li><a href="#">Оплата</a></li>
+		      	<li><a href="#">Возврат товара</a></li>
+		      	<li><a href="#">Отзывы клиентов</a></li>
+		      </ul>
+		      <ul class="ah-footer-list">
+		      	<li><a href="#">Дополнительно</a></li>
+		      	<li><a href="#">Таблица размеров</a></li>
+		      	<li><a href="#">Советы по уходу за обувью</a></li>
+		      	<li><a href="#">Обувь больших размеров</a></li>
+		      	<li><a href="#">Ответы на вопросы</a></li>
+		      </ul>
+		      <ul class="ah-footer-list">
+		      	<li><a href="#">О нас</a></li>
+		      	<li><a href="#">О компании</a></li>
+		      	<li><a href="#">Публичная оферта</a></li>
+		      	<li><a href="#">Сертификаты</a></li>
+		      	<li><a href="#">Фирменные магазины</a></li>
+		      </ul>
+		      <ul class="ah-footer-list">
+		      	<li><a href="#">Личный кабинет</a></li>
+		      	<li><a href="#">Вход / Регистрация</a></li>
+		      	<li><a href="#">Мои заказы</a></li>
+		      	<li><a href="#">Корзина</a></li>
+		      	<li><a href="#">Контакты</a></li>
+		      </ul>
+            </div>
+            <div class="col-md-5 col-sm-5 col-xs-12 ">
+				<div class="ah-logoblock">
+					<div class="ah-logoimg">
+						<img src="img/footer-logo.png" alt="footer-logo">
+					</div>
+					<span class="ah-logoblock-span"><span>8 (800)</span> 707-01-81</span>
+				</div>
+				<form class="ah-footform">
+					<label>
+						<input type="text" name="poisk">
+						<input type="submit" value="Найти">
+					</label>
+				</form>
+				<div class="ah-fotbotblock">
+					<div class="ah-fotbotblock1">
+						<span class="ah-fotbotblock1-span">Мы принимаем</span>
+						<div class="ah-cardimg">
+							<img src="img/ico-cards.png" alt="ico-cards.png">
+						</div>
+					</div>
+					<div class="ah-fotbotblock2">
+						<span class="ah-fotbotblock2-span">Мы в</span>
+						<ul class="ah-soc">
+							<li><a href="#" class="ah-vk"></a></li>
+							<li><a href="#" class="ah-in"></a></li>
+						</ul>
+					</div>
+				</div>
+            </div>
+        </div>
+	</div>
+</section>
