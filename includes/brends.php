5c15535c51c3e8e3507d7ea87d145d772d925530
engine77 <zaur.ka.00@mail.ru>
2016-10-22 16:36:13 +0300

    b

---
+++ includes/brends.php
@@ -0,0 +1,23 @@
+<div class="container ">
+	<div class="zk-caption">
+		<h3 class="zk-brends-caption">Бренды</h3>
+		<span class="zk-liliya"><img src="/img/ico-new.png" alt=""></span>
+		<div class="clear"></div>
+	</div>
+
+	<div class="col-md-4">
+		<div class="zk-brends">
+			<img src="/img/zk-brend.png" alt="">
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="zk-brends">
+			<img src="/img/zk-brend2.png" alt="">
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="zk-brends">
+			<img src="/img/zk-brend3.png" alt="">
+		</div>
+	</div>			
+</div>
