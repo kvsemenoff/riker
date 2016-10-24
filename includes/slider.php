f3213c5e477f0d3bc1d671ac61f8f3cee05cdd56
Berzeg07 <berezg86@gmail.com>
2016-10-24 17:41:26 +0400

    17.10

--- includes/slider.php
+++ includes/slider.php
@@ -24,6 +24,27 @@
 							<a href="#">Подробнее</a>
 						</div>
 					</div>
+					<div class="item">
+						<img src="img/ab-slide-1.jpg" alt="">
+						<div class="slidetextbox">
+							<a href="#">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="ab-red">
+					<div class="ab-img-box2">
+						<span><a href="#">Бесплатная доставка и примерка (Самара)</a></span>
+					</div>
+					<div class="ab-img-box3 ">
+						<span><a href="#">Сеть розничных магазинов</a></span>
+					</div>
+					<div class="ab-img-box4 ">
+						<span><a href="#">Безналичная оплата Наложенный платеж</a></span>
+					</div>
+					<div class="ab-img-box5 ">
+						<span><a href="#">Возврат и обмен<br> без проблем</a></span>
+					</div>
+					<div class="clearfix"></div>
 				</div>
 			</div>
 			<div class="clearfix"></div>
