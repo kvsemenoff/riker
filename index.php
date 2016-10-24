3436fe44921370ca063368010c8aef8824df5385
Berzeg07 <berezg86@gmail.com>
2016-10-24 12:18:34 +0400

    17.10

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
 
 	<?php require_once('includes/header.php'); ?>
 	<?php require_once('includes/sidebar.php'); ?>
+	<?php require_once('includes/slider.php'); ?>
 	<?php require_once('includes/n-news.php'); ?>
 	<?php require_once('includes/brends.php'); ?>
 	<?php require_once('includes/aboutcompany.php'); ?>
