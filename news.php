e3528ead045581e9adb85f45267d1f5e04cec2f4
Berzeg07 <berezg86@gmail.com>
2016-10-24 18:44:34 +0400

    17.10

--- news.php
+++ news.php
@@ -51,13 +51,88 @@
 	<div class="clearfix"></div>
 </section>
 
-<section class="news">	
+<section class="ab-news-wrap">	
 	<div class="container">
 		<div class="row">
+			<div class="col-md-3">
 			<?php require_once('includes/sidebar.php'); ?>
-			<div class="col-md-9">
-
 			</div>
+			<div class="col-md-9">
+				<div class="ab-news">
+					<div class="ab-news-left">
+						<a href="#"><img src="img/ab-j.png" alt=""></a>
+					</div>
+					<div class="ab-news-right">
+						<div class="news-date">01.07.2016</div>
+						<div class="ab-news-main">
+							<span>Международная выставка одежды Salon De Fashion</span>
+							<span>В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. На выставке Salon De Fashion будут представлены все типы одежды для офиса и дома от платьев и шорт до VIP-аксессуаров, включая оборудование для производства и детской одежды.
+							В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. 
+							</span>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="ab-news">
+					<div class="ab-news-left">
+						<a href="#"><img src="img/ab-j.png" alt=""></a>
+					</div>
+					<div class="ab-news-right">
+						<div class="news-date">01.07.2016</div>
+						<div class="ab-news-main">
+							<span>Международная выставка одежды Salon De Fashion</span>
+							<span>В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. На выставке Salon De Fashion будут представлены все типы одежды для офиса и дома от платьев и шорт до VIP-аксессуаров, включая оборудование для производства и детской одежды.
+							В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. 
+							</span>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="ab-news">
+					<div class="ab-news-left">
+						<a href="#"><img src="img/ab-j.png" alt=""></a>
+					</div>
+					<div class="ab-news-right">
+						<div class="news-date">01.07.2016</div>
+						<div class="ab-news-main">
+							<span>Международная выставка одежды Salon De Fashion</span>
+							<span>В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. На выставке Salon De Fashion будут представлены все типы одежды для офиса и дома от платьев и шорт до VIP-аксессуаров, включая оборудование для производства и детской одежды.
+							В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. 
+							</span>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="ab-news">
+					<div class="ab-news-left">
+						<a href="#"><img src="img/ab-j.png" alt=""></a>
+					</div>
+					<div class="ab-news-right">
+						<div class="news-date">01.07.2016</div>
+						<div class="ab-news-main">
+							<span>Международная выставка одежды Salon De Fashion</span>
+							<span>В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. На выставке Salon De Fashion будут представлены все типы одежды для офиса и дома от платьев и шорт до VIP-аксессуаров, включая оборудование для производства и детской одежды.
+							В следующем  году выставка Salon De Fashion обещает быть еще более расширенной и интересной. 
+							</span>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<hr>
+				<div class="ab-pagination">
+					<ul>
+						<li class="control">
+							<a href="#"><img src="img/slide-prev.png" alt=""></a>
+						</li>
+						<li><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li class="control">
+							<a href="#"><img src="img/slide-next.png" alt=""></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 </section>
