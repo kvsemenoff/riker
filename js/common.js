82c0f657b498e665645b97bcb54fcf5d8115c9c4
engine77 <zaur.ka.00@mail.ru>
2016-10-26 13:53:46 +0300

    25

--- js/common.js
+++ js/common.js
@@ -2,68 +2,49 @@
 
 $(document).ready(function(){
 
-    $('.az-select').each(function(){
-        var select = $(this);    
-        var option = select.find('select option');
-        var str = '<div class="az-options">';
-        select.find('option').each(function(){
-            str+= '<div data-val="' +$(this).val() + '">' + $(this).text() + '</div>'
-        });
-        str+= '</div>';
-        select.html(select.html() + str);
+    // $('.az-select').each(function(){
+    //     var select = $(this);    
+    //     var option = select.find('select option');
+    //     var str = '<div class="az-options">';
+    //     select.find('option').each(function(){
+    //         str+= '<div data-val="' +$(this).val() + '">' + $(this).text() + '</div>'
+    //     });
+    //     str+= '</div>';
+    //     select.html(select.html() + str);
 
-        select.find('select').mousedown(function(){
-            return false;
-        });
-        select.mouseup(function(){
-            select.find('select').focus();
-        });
-        select.find('.az-options div[data-val]').click(function(){
-            select.find('select').val($(this).attr('data-val'));
-        });
-        select.find('select').focusout(function(){
-            if(!select.is(':hover')){
-                select.find('.az-options').slideUp(0);
-                select.removeClass('az-select-focus');
-            }
-        });
-    });
+    //     select.find('select').mousedown(function(){
+    //         return false;
+    //     });
+    //     select.mouseup(function(){
+    //         select.find('select').focus();
+    //     });
+    //     select.find('.az-options div[data-val]').click(function(){
+    //         select.find('select').val($(this).attr('data-val'));
+    //     });
+    //     select.find('select').focusout(function(){
+    //         if(!select.is(':hover')){
+    //             select.find('.az-options').slideUp(0);
+    //             select.removeClass('az-select-focus');
+    //         }
+    //     });
+    // });
 
-    $(".az-select").click(function(){
-        $(this).find('.az-options').slideToggle(0);
-        $(this).toggleClass('az-select-focus');
-    });
+    // $(".az-select").click(function(){
+    //     $(this).find('.az-options').slideToggle(0);
+    //     $(this).toggleClass('az-select-focus');
+    // });
 
 
     //====slidemenu===category.php=======
 
 
-// <<<<<<< HEAD
-//     // $('.item').hover(function(){
-//     //      $('.zk-show-menu', this).slideDown(500);
-//     $('.zk-category').hover(function(){
-//         $('.zk-show-menu', this).slideDown(450);
-// =======
-//     $('.zk-category').hover(function(){
-//         $('.zk-show-menu', this).slideDown(500);    
 
-//     $('.zk-category').hover(function(){
-//         $('.zk-show-menu', this).slideDown(450);    
-
-// >>>>>>> 7393d275559ac39150e787502483f64e77fe6b8f
-//     },function(){
-//         $('.zk-show-menu', this).slideUp(400);             
-//     });
-//  //});
 
 
 $('.zk-category').hover(function(){
-  $('.zk-show-menu', this).slideToggle(450);
+  $('.zk-show-menu',this).slideToggle(450);
 });
 
-// },function(){
-//   $('.zk-show-menu', this).slideUp(400);        
-// });
 
 
 
@@ -71,7 +52,7 @@ $('.zk-category').hover(function(){
 
 
 
-$(document).ready(function() {
+
  
   var owl = $("#owl-demo");
  
@@ -100,30 +81,14 @@ $(document).ready(function() {
           items:6
         }
     }
-      // items : 6, //10 items above 1000px browser width
-      // itemsDesktop : [1000,5], //5 items between 1000px and 901px
-      // itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      // itemsTablet: [250,2], //2 items between 600 and 0
-      // // itemsMobile : true // itemsMobile disabled - inherit from itemsTablet option
+     
   });
  
-  // Custom Navigation Events
-  //  $(".next").click(function(){
-  //   owl.trigger('owl.next');
-  // })
-  //  $(".prev").click(function(){
-  //   owl.trigger('owl.prev');
-  // })
-  // $(".play").click(function(){
-  //   owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  // })
-  // $(".stop").click(function(){
-  //   owl.trigger('owl.stop');
-  // })
+  
  
-});
 
-$(document).ready(function() {
+
+
  
   var owl = $("#owl-demo2");
  
@@ -152,30 +117,14 @@ $(document).ready(function() {
           items:3
         }
     }
-      // items : 6, //10 items above 1000px browser width
-      // itemsDesktop : [1000,5], //5 items between 1000px and 901px
-      // itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      // itemsTablet: [250,2], //2 items between 600 and 0
-      // // itemsMobile : true // itemsMobile disabled - inherit from itemsTablet option
+     
   });
  
-  // Custom Navigation Events
-  //  $(".next").click(function(){
-  //   owl.trigger('owl.next');
-  // })
-  //  $(".prev").click(function(){
-  //   owl.trigger('owl.prev');
-  // })
-  // $(".play").click(function(){
-  //   owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  // })
-  // $(".stop").click(function(){
-  //   owl.trigger('owl.stop');
-  // })
+  
  
-});
 
-$(document).ready(function() {
+
+
  
   var owl = $("#owl-demo3");
  
@@ -203,26 +152,7 @@ $(document).ready(function() {
          1200:{
           items:3
         }
-    }
-      // items : 6, //10 items above 1000px browser width
-      // itemsDesktop : [1000,5], //5 items between 1000px and 901px
-      // itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      // itemsTablet: [250,2], //2 items between 600 and 0
-      // // itemsMobile : true // itemsMobile disabled - inherit from itemsTablet option
-  });
+    } 
  
-  // Custom Navigation Events
-  //  $(".next").click(function(){
-  //   owl.trigger('owl.next');
-  // })
-  //  $(".prev").click(function(){
-  //   owl.trigger('owl.prev');
-  // })
-  // $(".play").click(function(){
-  //   owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  // })
-  // $(".stop").click(function(){
-  //   owl.trigger('owl.stop');
-  // })
- 
-});
+ });
+ }); 
